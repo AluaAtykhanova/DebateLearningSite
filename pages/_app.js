5673d3cba08a366796e390aa8c4ed6eb32d191ed
AluaAtykhanova <30786@iitu.edu.kz>
2023-07-19 13:38:35 +0600

    Change head

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Debateland</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/icon2.png" />
+      <link rel="icon" href="/icon.png" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
