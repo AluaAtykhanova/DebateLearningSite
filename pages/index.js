613d2a6a29bd61d1bc3b423221ba79e29378e667
AluaAtykhanova <30786@iitu.edu.kz>
2023-07-26 16:50:58 +0600

    Deploy 2

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ const Home = () => (
       <div className="gradient-03 z-0" />
       <Explore />
     </div>
-    {/* <div className="relative">
+    { 
+    /* <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
       <WhatsNew />
@@ -21,8 +22,9 @@ const Home = () => (
       <Insights />
       <div className="gradient-04 z-0" />
       <Feedback />
-    </div >/*/
-    <Footer />}
+    </div > */
+    <Footer />
+    }
   </div>
 );
 
