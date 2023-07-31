471ce48e2cd7309b6943fa41b8b2d1dfa1ec392f
AluaAtykhanova <30786@iitu.edu.kz>
2023-07-31 09:12:14 +0600

    2023

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -9,7 +9,7 @@ const ComingSoon = () => (
       <div className="mb-8 text-center">
         <img src="/icon_white.png" alt="N17R" className="w-20 mx-auto" />
         <h2 className="mt-2 text-3xl font-bold md:text-4xl">
-          nFactorial Incubator 2024
+          nFactorial Incubator 2023
         </h2>
       </div>
       <div className="mt-2 text-xl text-center md:text-3xl">Coming soon</div>
