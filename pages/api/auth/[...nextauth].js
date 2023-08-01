e17c615425d0cddf2522acebd92c1c31697c31ef
AluaAtykhanova <30786@iitu.edu.kz>
2023-08-01 09:56:04 +0600

    Deploy auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,19 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+require('dotenv').config();
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.JWT_SECRET,
    callbacks: {
       async signIn({ account, profile }) {
         if (account.provider === "google") {
-          return profile.email_verified && profile.email.endsWith("@example.com")
+          return profile.email_verified //&& profile.email.endsWith("@gmail.com")
         }
         return true // Do different verification for other providers that don't have `email_verified`
       },
