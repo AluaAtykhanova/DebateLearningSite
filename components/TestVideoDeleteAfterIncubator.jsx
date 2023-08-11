01d03f43f1a33ff8b72b43456829482a2c06dec1
AluaAtykhanova <30786@iitu.edu.kz>
2023-08-11 08:26:17 +0600

    course mobile

--- components/TestVideoDeleteAfterIncubator.jsx
+++ components/TestVideoDeleteAfterIncubator.jsx
@@ -1,149 +1,129 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
-
-const Video = () => {
-  const [windowWidth, setWindowWidth] = useState(
-    typeof window !== 'undefined' ? window.innerWidth : 500,
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return (
-    <div className="mx-auto max-w-7xl" style={{ color: 'white', minHeight: '100vh' }}>
-      <div className="px-4 py-8 mx-auto max-w-screen-xl">
-        <div className="mb-8 text-center">
-          <img src="/icon_white.png" alt="N17R" className="w-20 mx-auto" />
-          <h2 className="mt-2 text-3xl font-bold md:text-4xl">
-            nFactorial Incubator 2023
-          </h2>
-        </div>
-        <div className="mt-2 text-xl text-center md:text-3xl">Coming soon</div>
-        <div className={`mt-8 ${windowWidth >= 820 ? 'grid grid-cols-5 gap-4' : 'flex md:flex-row flex-col gap-4'}`}>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <a
-              href="https://www.youtube.com/watch?v=FY6U5JZmib0&ab_channel=lenagotlib"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/Debate5.jpg"
-                alt="Video Thumbnail"
-                className="w-full h-auto rounded-md"
-              />
-            </a>
-            <h3 className="text-xl mb-2">За что мы так любим дебаты?</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <a
-              href="https://www.youtube.com/watch?v=3LS68WfC_jY&ab_channel=lenagotlib"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/Debate3.jpg"
-                alt="Формат игры в Дебаты"
-                className="w-full h-auto rounded-md"
-              />
-            </a>
-            <h3 className="text-xl mb-2">Формат проведения Дебат(БПФ)</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <a
-              href="https://www.youtube.com/watch?v=Sd5PHXC-Tvs&ab_channel=%D0%94%D0%B5%D0%B1%D0%B0%D1%82%D0%BB%D1%8F%D0%BD%D0%B4%D0%B8%D1%8F"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/Debate3.jpg"
-                alt="Формат игры в Дебаты"
-                className="w-full h-auto rounded-md"
-              />
-            </a>
-            <h3 className="text-xl mb-2">Разбор показательной игры</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <a
-              href="https://www.youtube.com/watch?v=2h7IOttcxrY&ab_channel=IDEA%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F%D0%90%D0%B7%D0%B8%D1%8F"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/Debate3.jpg"
-                alt="Формат игры в Дебаты"
-                className="w-full h-auto rounded-md"
-              />
-            </a>
-            <h3 className="text-xl mb-2">Показательная игра</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <a
-              href="https://www.youtube.com/watch?v=EFsQJLeqCEw&ab_channel=%D0%A0%D0%A3%D0%9C%D0%A6%D0%94%D0%9E%D0%9C%D0%9F%D0%A0%D0%9A"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/Debate3.jpg"
-                alt="Формат игры в Дебаты"
-                className="w-full h-auto rounded-md"
-              />
-            </a>
-            <h3 className="text-xl mb-2">Формат Дебат (АПФ)</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <img
-              src="/Debate5.jpg"
-              alt="Video Thumbnail"
-              className="w-full h-auto rounded-md"
-            />
-            <h3 className="text-xl mb-2">Видео 1</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <img
-              src="/Debate3.jpg"
-              alt="Формат игры в Дебаты"
-              className="w-full h-auto rounded-md"
-            />
-            <h3 className="text-xl mb-2">Видео 2</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <img
-              src="/Debate3.jpg"
-              alt="Формат игры в Дебаты"
-              className="w-full h-auto rounded-md"
-            />
-            <h3 className="text-xl mb-2">Видео 3</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <img
-              src="/Debate3.jpg"
-              alt="Формат игры в Дебаты"
-              className="w-full h-auto rounded-md"
-            />
-            <h3 className="text-xl mb-2">Видео 4</h3>
-          </div>
-          <div className="bg-gray-800 p-4 rounded-md">
-            <img
-              src="/Debate3.jpg"
-              alt="Формат игры в Дебаты"
-              className="w-full h-auto rounded-md"
-            />
-            <h3 className="text-xl mb-2">Видео 5</h3>
-          </div>
-        </div>
+const Video = () => (
+  <div className="mx-auto max-w-7xl" style={{ color: 'white', minHeight: '100vh' }}>
+  <div className="px-4 py-8 mx-auto max-w-screen-xl">
+    <div className="mb-8 text-center">
+      <img src="/icon_white.png" alt="N17R" className="w-20 mx-auto" />
+      <h2 className="mt-2 text-3xl font-bold md:text-4xl">
+        nFactorial Incubator 2023
+      </h2>
+    </div>
+    <div className="mt-2 text-xl text-center md:text-3xl">Coming soon</div>
+    <div className={(typeof window === 'undefined' ? 500 : window.innerWidth) >= 900 ? 'mt-8 grid grid-cols-5 gap-4' : 'mt-8 grid-cols-2 md:flex gap-4'}>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <a
+          href="https://www.youtube.com/watch?v=FY6U5JZmib0&ab_channel=lenagotlib"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Debate5.jpg"
+            alt="Video Thumbnail"
+            className="w-full h-auto rounded-md"
+          />
+        </a>
+        <h3 className="text-xl mb-2">За что мы так любим дебаты?</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <a
+          href="https://www.youtube.com/watch?v=3LS68WfC_jY&ab_channel=lenagotlib"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Debate3.jpg"
+            alt="Формат игры в Дебаты"
+            className="w-full h-auto rounded-md"
+          />
+        </a>
+        <h3 className="text-xl mb-2">Формат проведения Дебат(БПФ)</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <a
+          href="https://www.youtube.com/watch?v=Sd5PHXC-Tvs&ab_channel=%D0%94%D0%B5%D0%B1%D0%B0%D1%82%D0%BB%D1%8F%D0%BD%D0%B4%D0%B8%D1%8F"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Debate3.jpg"
+            alt="Формат игры в Дебаты"
+            className="w-full h-auto rounded-md"
+          />
+        </a>
+        <h3 className="text-xl mb-2">Разбор показательной игры</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <a
+          href="https://www.youtube.com/watch?v=2h7IOttcxrY&ab_channel=IDEA%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F%D0%90%D0%B7%D0%B8%D1%8F"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Debate3.jpg"
+            alt="Формат игры в Дебаты"
+            className="w-full h-auto rounded-md"
+          />
+        </a>
+        <h3 className="text-xl mb-2">Показательная игра</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <a
+          href="https://www.youtube.com/watch?v=EFsQJLeqCEw&ab_channel=%D0%A0%D0%A3%D0%9C%D0%A6%D0%94%D0%9E%D0%9C%D0%9F%D0%A0%D0%9A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/Debate3.jpg"
+            alt="Формат игры в Дебаты"
+            className="w-full h-auto rounded-md"
+          />
+        </a>
+        <h3 className="text-xl mb-2">Формат Дебат (АПФ)</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <img
+          src="/Debate5.jpg"
+          alt="Video Thumbnail"
+          className="w-full h-auto rounded-md"
+        />
+        <h3 className="text-xl mb-2">Видео 1</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <img
+          src="/Debate3.jpg"
+          alt="Формат игры в Дебаты"
+          className="w-full h-auto rounded-md"
+        />
+        <h3 className="text-xl mb-2">Видео 2</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <img
+          src="/Debate3.jpg"
+          alt="Формат игры в Дебаты"
+          className="w-full h-auto rounded-md"
+        />
+        <h3 className="text-xl mb-2">Видео 3</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <img
+          src="/Debate3.jpg"
+          alt="Формат игры в Дебаты"
+          className="w-full h-auto rounded-md"
+        />
+        <h3 className="text-xl mb-2">Видео 4</h3>
+      </div>
+      <div className="bg-gray-800 p-4 rounded-md">
+        <img
+          src="/Debate3.jpg"
+          alt="Формат игры в Дебаты"
+          className="w-full h-auto rounded-md"
+        />
+        <h3 className="text-xl mb-2">Видео 5</h3>
       </div>
     </div>
-  );
-};
+  </div>
+  </div>
+);
 
 export default Video;
